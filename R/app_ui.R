b3d73c3921ce6e113dee08891b98a005d30ba93b
johnlecocq <johnlecocq@gmail.com>
2024-05-09 14:51:52 -0400

    Prototype of nutrients app

--- R/app_ui.R
+++ R/app_ui.R
@@ -3,14 +3,94 @@
 #' @param request Internal parameter for `{shiny}`.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import bslib
+#' @import shinyWidgets
 #' @noRd
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
     golem_add_external_resources(),
     # Your application UI logic
-    fluidPage(
-      h1("nutrientCalculator")
+    page_fluid(
+      theme = bs_theme(version = 5,
+                       bg = "white",
+                       fg = "black",
+                       primary = "royalblue",
+                       secondary = "goldenrod",
+                       danger = "red",
+                       heading_font = font_google("Bebas Neue"),
+                       base_font = font_google("Karla")),
+card(
+  card_header(
+    fluidRow(column(12,align='center', h1("LIST WHAT YOU ATE TODAY"))),
+fluidRow(
+  column(9,
+         align="center",
+      layout_column_wrap(
+        shinyWidgets::virtualSelectInput(
+          inputId = "food-1",
+          label = "FOOD",
+          choices = c("USDA foods list simplified"),
+          search = T
+        ),
+        shinyWidgets::numericInputIcon(
+          inputId = "amount-1",
+          label = "AMOUNT",
+          help_text = "Enter how much you ate (a number greater than zero)",
+          icon = icon('weight-scale'),
+          value = NULL,
+          min=0
+        ))
+      ),
+      column(3,
+             align='center',
+
+             fluidRow(column(8,align='left',
+                             layout_column_wrap(
+        shinyWidgets::virtualSelectInput(
+          multiple = F,
+          inputId = "unit-1",
+          label = "UNITS",
+          choices = c("lbs", "oz", "kg", "g", "fl oz"),
+          search = T
+        ))
+        ),
+        column(4, align='right',
+        shinyWidgets::actionBttn(inputId = "remove-food",
+                                 label = NULL,
+                                 color = "danger",
+                                 size = "sm",
+                                 style = "material-circle",
+                                 no_outline = T,
+                                 icon = icon("remove"))
+        ))
+      ))
+      ),
+
+layout_column_wrap(
+  shinyWidgets::actionBttn(inputId = "add-food",
+                           label = "Add",
+                           color = "primary",
+                           style = "unite",
+                           icon = icon("add"))
+),
+card_body(
+  fluidRow(column(12, align = 'center',
+                  h1("SEE HOW WELL YOU DID"))),
+fluidRow(column(8,
+                p("Plot")),
+         column(4,
+                p("Table"))
+),
+fluidRow(column(4, offset = 4, align = 'center',
+                layout_column_wrap(width = 1/5,
+                icon("star"),
+                icon("star"),icon("star"),
+                icon("star"),icon("star")
+                ))
+)
+)
+)
     )
   )
 }
