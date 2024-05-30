7f18b587a2d4e2bbe374eed344f3c9d26aa38894
johnlecocq <johnlecocq@gmail.com>
2024-05-30 16:38:26 -0400

    app updatd so that one may select a food, serving size and enter the number of servings. Major improvement was allowing the portion sizes to be catered to the arbitrary number of foods to select.

--- R/app_server.R
+++ R/app_server.R
@@ -3,9 +3,84 @@
 #' @param input,output,session Internal parameters for {shiny}.
 #'     DO NOT REMOVE.
 #' @import shiny
+#' @import shinyWidgets
+#' @import purrr
+#' @import dplyr
+#' @import stringr
+#' @import tidyr
 #' @noRd
+
+food_choices <- read.csv("./data/sr_legacy/food.csv") %>%
+  select(1,3,4) %>%
+  arrange(description)
+nutrient <- read.csv("./data/sr_legacy/nutrient_new.csv")
+food_nutrient <- read.csv("./data/sr_legacy/food_nutrient.csv") %>%
+  select(2,3,4)
+food_portion <- read.csv("./data/sr_legacy/food_portion.csv") %>%
+  select(2,4,7,8)
+req_data_raw <- read.csv("./data/rdi_data.csv", check.names=FALSE) %>%
+  pivot_longer(cols = 2:last_col(),
+               names_to = "name",
+               values_to = "amount")
+
 app_server <- function(input, output, session) {
-  # Your application server logic
+
+food_names <- reactive(paste0("food", seq_len(input$num_inputs)))
+portion_sizes <- reactive(paste0("potrion_size", seq_len(input$num_inputs)))
+
+req_data <- eventReactive(input$lifestage,{
+  req_data_raw %>%
+    filter(`Life Stage Group` == input$lifestage)
+})
+
+output$foods <- renderUI({
+  map(food_names(),
+      ~virtualSelectInput(
+        selected = isolate(input[[.x]]),
+        inputId = .x,
+        label = NULL,
+        choices = food_choices$description,
+        search = TRUE,
+        width = "100%",
+        markSearchResults = TRUE,
+        dropboxWrapper = "body"
+      ))
+  })
+
+portion_types_list <- reactive({
+  filtered_food_list <- map(seq_along(food_names()),
+                       ~filter(food_choices, description == input[[paste0("food", .x)]]))
+  portion_list <- map(seq_along(filtered_food_list),
+                      ~filter(food_portion, fdc_id == filtered_food_list[[.x]]$fdc_id) %>%
+                        unique()
+  )
+  portion_list
+})
+
+output$portion_types <- renderUI({
+  map(seq_along(food_names()),
+      ~virtualSelectInput(
+        showSelectedOptionsFirst = F,
+        inputId = paste0("portion", .x),
+        label = NULL,
+        choices = portion_types_list()[[.x]]$modifier,
+        search = TRUE,
+        width = "100%",
+        markSearchResults = TRUE,
+        dropboxWrapper = "body"
+      )
+        )
+})
+
+output$portion_sizes <- renderUI({
+  map(portion_sizes(),
+      ~numericInput(inputId = .x,
+                    label = NULL,
+                    width = "100%",
+                    value = isolate(input[[.x]]),
+                    min = 0)
+        )
+})
 
 
 }
--- R/app_ui.R
+++ R/app_ui.R
@@ -5,7 +5,11 @@
 #' @import shiny
 #' @import bslib
 #' @import shinyWidgets
+#' @import dplyr
 #' @noRd
+
+lifestages <- read.csv("./data/lifestages.csv", check.names=FALSE)
+
 app_ui <- function(request) {
   tagList(
     # Leave this function for adding external resources
@@ -22,61 +26,38 @@ app_ui <- function(request) {
                        base_font = font_google("Karla")),
 card(
   card_header(
-    fluidRow(column(12,align='center', h1("LIST WHAT YOU ATE TODAY"))),
-fluidRow(
-  column(9,
-         align="center",
-      layout_column_wrap(
-        shinyWidgets::virtualSelectInput(
-          inputId = "food-1",
-          label = "FOOD",
-          choices = c("USDA foods list simplified"),
-          search = T
-        ),
-        shinyWidgets::numericInputIcon(
-          inputId = "amount-1",
-          label = "AMOUNT",
-          help_text = "Enter how much you ate (a number greater than zero)",
-          icon = icon('weight-scale'),
-          value = NULL,
-          min=0
-        ))
-      ),
-      column(3,
-             align='center',
-
-             fluidRow(column(8,align='left',
-                             layout_column_wrap(
-        shinyWidgets::virtualSelectInput(
-          multiple = F,
-          inputId = "unit-1",
-          label = "UNITS",
-          choices = c("lbs", "oz", "kg", "g", "fl oz"),
-          search = T
-        ))
-        ),
-        column(4, align='right',
-        shinyWidgets::actionBttn(inputId = "remove-food",
+    fluidRow(column(6, align = 'center',
+                    h3("1) DESCRIBE THE PERSON THIS DIETARY ANALYSIS IS FOR:"),
+                    virtualSelectInput("lifestage",
+                                       label = NULL,
+                                       inline = T,
+                                       width = "100%",
+                                       choices = lifestages$lifestages)),
+             column(6, align = 'center',
+                    h3("2) HOW MANY INDIVIDUAL FOODS DID THEY EAT?"),
+                    numericInputIcon("num_inputs",
+                                 icon = icon("carrot"),
                                  label = NULL,
-                                 color = "danger",
-                                 size = "sm",
-                                 style = "material-circle",
-                                 no_outline = T,
-                                 icon = icon("remove"))
-        ))
-      ))
-      ),
-
-layout_column_wrap(
-  shinyWidgets::actionBttn(inputId = "add-food",
-                           label = "Add",
-                           color = "primary",
-                           style = "unite",
-                           icon = icon("add"))
-),
+                                 min = 1, step = 1, value = 1))),
+    ),
 card_body(
+  div(style = "color:green;",
+      fluidRow(column(12, align = 'center',
+                      h3("3) DESCRIBE THE FOODS AND THEIR AMOUNTS AS ACCURATELY AS POSSIBLY")
+                      )
+               ),
+      fluidRow(column(6, align = 'left',
+                      p("Food"),
+                      htmlOutput("foods")),
+               column(3, align = 'center',
+                      p("Number of Servings"),
+                      htmlOutput("portion_sizes")),
+               column(3, align = 'right',
+                      p("Serving Size"),
+                      htmlOutput("portion_types")))
+  ),
   fluidRow(column(12, align = 'center',
-                  h1("SEE HOW WELL YOU DID"))),
+                  h3("SEE HOW WELL YOU DID"))),
 fluidRow(column(8,
                 p("Plot")),
          column(4,
